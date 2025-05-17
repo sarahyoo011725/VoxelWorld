4cdb40cd4bf5014ad9b04b747bdf737391d5ca88
Gaeun Yoo <gaeunyoo3626@gmail.com>
2025-05-17 18:48:27 -0400

    note about vbo and ebo init

--- VBO.h
+++ VBO.h
@@ -4,13 +4,23 @@
 
 using namespace std;
 
+/*
+	Technical Note:
+	It is better to manually initialize vbo and ebo with their array byte size as a parameter.
+	Specifying their size in constructor by default causes some byte allocating issue, which makes nothing drawn on the window.
+	To calculate byte size of array or vector:
+	- vector: sizeof(element type) * vector.size()
+	- array: sizeof(array)
+	To calculate array length in the other way:
+	- sizeof(array) / sizeof(element type)
+*/
+
 class VBO
 {
 public:
 	GLuint id;
-	VBO(vector<vertex> vertices, GLsizeiptr size);
+	VBO(vector<vertex> vertices, GLsizeiptr size); 
 	void bind();
 	void unbind();
 	void destroy();
 };
-
